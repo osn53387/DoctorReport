525ace2b573f727ef33416c2b92e065e2eab2853
Owen Na <newseoul1302@gmail.com>
2022-09-23 19:48:28 -0400

    Added run counting stage and updated dependencies.

--- app/src/main/java/com/example/doctorreport/MainActivity.java
+++ app/src/main/java/com/example/doctorreport/MainActivity.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.text.method.LinkMovementMethod;
 import android.widget.Button;
+import android.widget.TextView;
+
+import java.text.DateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     Button syncButton;
+
+    TextView time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +24,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         syncButton = findViewById(R.id.SyncButton);
+        time = findViewById(R.id.Time);
 
         toSyncingPage();
+        updateRealTime();
     }
 
     private void toSyncingPage() {
         syncButton.setMovementMethod(LinkMovementMethod.getInstance());
         syncButton.setOnClickListener(view -> {
             startActivity(new Intent(MainActivity.this, DataSync.class));
+            finish();
         });
+    }
+
+    private void updateRealTime() {
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                // display your time here...
+                String currentDateTimeString = DateFormat.getDateTimeInstance().format(new Date());
+                time.setText(currentDateTimeString);
+            }
+        }, 1000); // here 1 seconds to refresh time after 1 seconds
     }
 
 }
